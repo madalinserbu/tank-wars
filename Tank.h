089f322d828454a0f8b3b946f8f2843047c9e6ab
Serbu Ovidiu-Madalin <92184535+madalinserbu@users.noreply.github.com>
2024-11-16 22:02:13 +0200

    Update Tank.h

---
+++ Tank.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "core/gpu/mesh.h"
+#include "utils/glm_utils.h"
+
+namespace object2D {
+
+    // Func?ie care creeaz? baza tancului (dou? trapeze suprapuse)
+    Mesh* CreateTankBase(const std::string& name, const glm::vec3& color);
+
+    // Func?ie care creeaz? turela tancului (arc de cerc)
+    Mesh* CreateTankTurret(const std::string& name, const glm::vec3& color);
+
+    // Func?ie care creeaz? ?eava tunului (dreptunghi)
+    Mesh* CreateTankBarrel(const std::string& name, const glm::vec3& color);
+
+    // Func?ia principal? pentru a crea un tanc complet (include baza, turela ?i ?eava)
+    void CreateTank(const std::string& name, const glm::vec3& baseColor, const glm::vec3& turretColor, std::vector<Mesh*>& tankMeshes);
+    Mesh* CreateProjectile(const std::string& name, float radius, const glm::vec3& color);
+}
